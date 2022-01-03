f827f77c651a9a18f6b90d62744631aff8353773
Trinacu <aleks.tratnik@yahoo.com>
2022-01-03 13:34:30 +0100

    set5

--- set5.py
+++ set5.py
@@ -33,105 +33,110 @@ if run[0]:
     else:
         print('incorrect')
 
-
-
-class DH_Comm():
-    def __init__(self, p, g):
-        self.A = self.Entity("Alice", p, g)
-        self.B = self.Entity("Bob", p, g)
-        self.M = self.Entity("attacker", p, g)
-
+class Entity():
+    def __init__(self, channel, name, p, g):
+        self.name = name
         self.p = p
         self.g = g
-
-    def a2b(self, msg):
-        # Alice sends her public key
-        self.A.send(self.B, self.A.own_publ_key)
-        self.B.other_publ_key = self.B._data
-
-        # Bob sends his public key
-        self.B.send(self.A, self.B.own_publ_key)
-        self.A.other_publ_key = self.A._data
-
-        # Alice sends message
-        s = (self.A.other_publ_key ** self.A.priv_key) % self.p
-        sha = SHA1Hash(s.to_bytes(256, 'little'))
-        IV = get_random_bytes(16)
-        key = bytes.fromhex(sha.digest())[:16]
-        ciphertext = aes_cbc_encrypt(msg, key, IV)
-
-        self.A.send(self.B, ciphertext + IV)
+        self.priv_key = np.random.randint(2**8) % self.p
+        self.own_publ_key = (self.g ** self.priv_key) % self.p
+        self.other_publ_key = None
         
+        self._data = None
+        self.message = ""
 
-    def b2a(self, msg):
-        # Bob sends her public key
-        self.B.send(self.A, self.B.own_publ_key)
-        self.A.other_publ_key = self.A._data
+        self.channel = channel
 
-        # Alice sends his public key
-        self.A.send(self.B, self.A.own_publ_key)
-        self.B.other_publ_key = self.B._data
-
-        # Alice sends message
-        s = (self.B.other_publ_key ** self.B.priv_key) % self.p
+    def send_message(self, msg):
+        other = self.channel.A if self == self.channel.B else self.channel.B
+        s = (self.other_publ_key ** self.priv_key) % self.p
         sha = SHA1Hash(s.to_bytes(256, 'little'))
-        IV = get_random_bytes(16)
         key = bytes.fromhex(sha.digest())[:16]
+        IV = get_random_bytes(16)
         ciphertext = aes_cbc_encrypt(msg, key, IV)
 
-        self.B.send(self.A, ciphertext + IV)
+        self.send_data(ciphertext + IV)
 
-    class Entity():
-        def __init__(self, name, p, g):
-            self.name = name
-            self.p = p
-            self.priv_key = np.random.randint(2**8) % p
-            self.own_publ_key = (g ** self.priv_key) % p
-            self.other_publ_key = None
-            
-            self._data = None
+    def send_data(self, data):
+        other = self.channel.A if self == self.channel.B else self.channel.B
+        other.receive(data)
 
-        def send(self, other, data):
-            other.receive(data)
-
-        def receive(self, data):
-            self._data = data
-
-        def decrypt_msg(self):
-            if not isinstance(self._data, bytes):
-                print("{}'s _data is not bytes object!".format(self.name))
+    def receive(self, data):
+        # received p, g, publ_key
+        if isinstance(data, list):
+            if len(data) != 3:
+                print('invalid message list length!')
                 return
+            self.p = data[0]
+            self.g = data[1]
+            self.other_publ_key = data[2]
+        # received key
+        if isinstance(data, int):
+            self.other_publ_key = data
+        # received message
+        if isinstance(data, bytes):
+            self._data = data
+            # separate data and IV and decrypt
             IV = self._data[-16:]
-            self._data = self._data[:-16]
+            data = self._data[:-16]
             s = (self.other_publ_key ** self.priv_key) % self.p
             sha = SHA1Hash(s.to_bytes(256, 'little'))
             key = bytes.fromhex(sha.digest())[:16]
-            data = aes_cbc_decrypt(self._data, key, IV)
-            return data
-            
+            self.message = aes_cbc_decrypt(data, key, IV)
+
+class DH_Comm():
+    def __init__(self, p, g, nameA, nameB):
+        self.A = Entity(self, nameA, p, g)
+        self.B = Entity(self, nameB, p, g)
+        self.p = p
+        self.g = g
+
+
+
 if run[1]:
     print("\n-----------")
     print("Challenge 34 - Implement MITM key-fixing attack on Diffie-Hellman")
+    
+    p = big_p
+    g = 2
+    #p = 37
+    #g = 5
+    data = b'test'
 
     """
-    s = diffie_hellman()
-    data = s.to_bytes(256, 'little')
-    tmp = SHA1Hash(data)
-    print(tmp.digest())
+    channelAB = DH_Comm(p, g, "Alice", "Bob")
+    print("Message: {}".format(channelAB.A.message))
+    channelAB.A.send_data([p, g, channelAB.A.own_publ_key])
+    channelAB.B.send_data(channelAB.B.own_publ_key)
+    channelAB.A.send_message(b'test')
+    channelAB.B.send_message(channelAB.B.message)
+    print("Message: {}".format(channelAB.A.message))
     """
     
-    #p = big_p
-    #g = 2
-    p = 37
-    g = 5
+    # M - middle man with 2 channels
+    channelAM = DH_Comm(p, g, "Alice", "middle")
+    channelMB = DH_Comm(p, g, "middle", "Bob")
+    A = channelAM.A
+    B = channelMB.B
+    MA = channelAM.B
+    MB = channelMB.A
+    print("A Message: {}".format(A.message))
     
-    channel = DH_Comm(p, g)
-    channel.a2b(b'test')
-    print(channel.B.decrypt_msg())
+    A.send_data([p, g, channelAM.A.own_publ_key])
+    MB.send_data([p, g, p])
+    B.send_data(B.own_publ_key)
+    MA.send_data(p)
+    A.send_message(data)
+    # relay data received on channelAM to B
+    MB.send_data(MA._data)
+    B.send_message(B.message)
+    MA.send_data(MA._data)
+    print("A Message: {}".format(A.message))
 
+    
 
-    channel.b2a(b'asdasd')
-    print(channel.A.decrypt_msg())
+    print("M Message: {}".format(channelAM.B.message))
+
 
 
     
